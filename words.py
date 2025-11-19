baff31508bf0926478ead639da20ae180ada5681
Seth <seth@dell_laptop.(none)>
2025-11-19 14:33:47 -0500

    first draft

---
+++ words.py
@@ -0,0 +1,5 @@
+def num_letters(num_rows,num_columns):
+    return(data[num_rows,num_columns])
+    
+assert num_letters(3, 4) == 10  #3 is the num_rows and 4 is the num_columns
+print("assertions checked.")
