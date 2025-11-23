2bcd00a7e1920cfa117c7734f91a2a9b8d4c301a
Seth <seth@dell_laptop.(none)>
2025-11-23 11:31:33 -0500

    uploading from computer

--- words.py
+++ words.py
@@ -10,36 +10,40 @@ r1=1
 r2=1
 while c1 < columns:
     ls.append(str(data[0,c1]))
-    c1 += c1
+    c1 += 1
 while c2 < columns:
-    ls.append(str(data[5,c2]))
-    c2 += c2
+    ls.append(str(data[rows-1,c2]))
+    c2 += 1
 while r1 < rows-1:
     ls.append(str(data[r1,0]))
-    r1 += r1
-while r1 < rows-1:
-    ls.append(str(data[r2,6]))
-    r2 += r2
+    r1 += 1
+while r2 < rows-1:
+    ls.append(str(data[r2,columns-1]))
+    r2 += 1
 print(ls)
-assert len(words) == num_letters(rows, columns)
+print(len(ls))
+print(num_letters(rows,columns))
+assert len(ls) == num_letters(rows, columns)
 words = [] 
 rows, columns = data.shape  
 c1=0
 c2=0
-r1=1
-r2=1
+r1=0
+r2=0
 while c1 < columns-1:
     words.append(str(data[0,c1])+str(data[0,c1+1]))
     c1 += 1
 while c2 < columns-1:
-    words.append(str(data[5,c2])+str(data[5,c2+1]))
+    words.append(str(data[rows-1,c2])+str(data[rows-1,c2+1]))
     c2 += 1
-while r1 < rows-2:
+while r1 < rows-1:
     words.append(str(data[r1,0])+str(data[r1+1,0]))
     r1 += 1
-while r2 < rows-2:
-    words.append(str(data[r2,6])+str(data[r2+1,6]))
+while r2 < rows-1:
+    words.append(str(data[r2,columns-1])+str(data[r2+1,columns-1]))
     r2 += 1
 print(words)
+print(len(words))
+print(num_letters(rows,columns))
 assert len(words) == num_letters(rows, columns)
 print('assertment done')
